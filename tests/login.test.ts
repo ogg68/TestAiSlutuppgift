bbe966402c7e5255faacee9a4cffb3afe6e20cbf
ola-gran <57948855+ola-gran@users.noreply.github.com>
2025-11-24 20:31:52 +0100

    login tests added

--- tests/login.test.ts
+++ tests/login.test.ts
@@ -1,17 +1,44 @@
 import { expect, test } from '@playwright/test';
 import { LoginPage } from "../pages/loginPage";
 
+//Username: any name...
+//Password: STORE_PASSWORD from playwright.yml and from Github.com/Settings/Actions secrets/Repo Secret
 let password: string;
 
-test('Succesful Login', async ({ page }) => {
+test("Login Fail wrongPwd", async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    await loginPage.goto();
+    await loginPage.login("wrongUser", "wrongPwd", "Consumer");
+
+    await expect(page.getByTestId("error-message")).toContainText("Incorrect password");
+  });
+
+
+test("Login Succesful Consumer", async ({ page }) => {
     const loginPage = new LoginPage(page);
     await loginPage.goto();
     if (process.env.STORE_PASSWORD !== undefined){
         password = process.env.STORE_PASSWORD;
     }
-    console.log("PWD:", password);
-    await loginPage.login('joe', password, "Consumer");
+    //await loginPage.login("joe", password, "Consumer");
+    await loginPage.login("joe", "sup3rs3cr3t", "Consumer");
     
-    //await loginPage.assertSuccessfulLogin();
     await expect(page).toHaveURL("/store2/");
   });
+
+
+  test("Logout Succesful", async({page}) => {
+    //first do login:
+    const loginPage = new LoginPage(page);
+    await loginPage.goto();
+    if (process.env.STORE_USER_PASSWORD !== undefined){
+        password = process.env.STORE_USER_PASSWORD;
+    }
+    //await loginPage.login("joe", password, "Consumer");
+    await loginPage.login("joe", "sup3rs3cr3t", "Consumer");
+    await expect(page).toHaveURL("/store2/");
+
+    //then do logout:
+    await loginPage.logout();
+    await expect(page).toHaveURL("/login/");
+  })
